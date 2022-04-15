0364d86bbd2a7d1de5ad55e35aa8a116e52719d0
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2022-04-15 22:30:16 +0300

    Enable CI  with Github actions

--- src/extension/providers/tree.ts
+++ src/extension/providers/tree.ts
@@ -23,7 +23,7 @@ export class ScratchTreeProvider implements TreeDataProvider<Scratch> {
   private _onDidChangeTreeData = new EventEmitter<Scratch | undefined>();
   readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
 
-  constructor(private readonly fileSystem: ScratchFileSystemProvider) { }
+  constructor(private readonly fileSystem: ScratchFileSystemProvider) {}
 
   getTreeItem(element: Scratch): TreeItem {
     return element;

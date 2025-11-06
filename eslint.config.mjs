96e405308e7721c752739e6cf6103a9bc7605ebd
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2025-11-06 21:48:14 +0200

    Dependencies housekeeping

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.nodeBuiltin },
   },
   tseslint.configs.recommended,
 ]);

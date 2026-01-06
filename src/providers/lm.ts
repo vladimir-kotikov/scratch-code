dacb73bd07ee9e9c9a1f9177640f8d6976916bb3
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2026-01-07 01:42:33 +0300

    Simplify tool registration logic and remove redundant error handling

--- src/providers/lm.ts
+++ src/providers/lm.ts
@@ -1,20 +1,12 @@
-import * as vscode from "vscode";
-import { map, prop } from "../util/fu";
-import { ScratchTreeProvider } from "./tree";
-
 import { Minimatch } from "minimatch";
-import {
-  LanguageModelDataPart,
-  LanguageModelTextPart,
-  LanguageModelTool,
-  LanguageModelToolResult,
-  PreparedToolInvocation,
-  Uri,
-} from "vscode";
+import * as vscode from "vscode";
+import { LanguageModelDataPart, LanguageModelTextPart, LanguageModelToolResult, Uri } from "vscode";
 import { DisposableContainer } from "../util/containers";
+import { map, prop } from "../util/fu";
 import { asPromise } from "../util/promises";
 import { splitLines, splitWords } from "../util/text";
 import { ScratchFileSystemProvider } from "./fs";
+import { ScratchTreeProvider } from "./tree";
 
 type ListScratchesOptions = {
   filter?: string;
@@ -51,9 +43,8 @@ export class ScratchLmToolkit extends DisposableContainer {
       .then(map(prop("uri")))
       .then(uris => {
         uris = pattern ? uris.filter(u => pattern.match(u.path)) : uris;
-        return uris.join("\n");
-      })
-      .then(text => (text === "" ? "No scratches found." : text));
+        return uris.map(u => strip(uriPath(u), ["/"])).join("\n");
+      });
   };
 
   readScratch = ({ uri, lineFrom, lineTo }: ReadScratchOptions) =>
@@ -72,11 +63,28 @@ export class ScratchLmToolkit extends DisposableContainer {
       .writeFile(uri instanceof Uri ? uri : Uri.parse(uri), content, {
         create: true,
         overwrite: true,
-      })
-      .then(() => "Scratch written successfully.");
+    });
 }
 
-export const registerTool = <P, R extends MaybePromiseLike<MaybeArray<LmResponsePart | string>>>(
+const maybeCall = <T, U>(val: T | ((arg: U) => T), arg: U): T => {
+  return typeof val === "function" ? (val as (arg: U) => T)(arg) : val;
+};
+
+const toToolResult = (res: MaybeArray<LmResponsePart | string | void>) => {
+  if (!Array.isArray(res)) {
+    res = [res] as Array<LmResponsePart | string | void>;
+  }
+  res = res
+    .map(res => (typeof res === "string" ? new LanguageModelTextPart(res) : res))
+    .filter(res => res !== undefined);
+
+  return new LanguageModelToolResult(res as LmResponsePart[]);
+};
+
+export const registerTool = <
+  P,
+  R extends MaybePromiseLike<MaybeArray<LmResponsePart | string | void>>,
+>(
   name: string,
   impl: (params: P) => R,
   params?: {
@@ -85,42 +93,16 @@ export const registerTool = <P, R extends MaybePromiseLike<MaybeArray<LmResponse
       | { title: string; message: string }
       | ((params: P) => { title: string; message: string });
   },
-) => {
-  const tool: LanguageModelTool<P> = {
-    invoke: ({ input }) => {
-      const result = asPromise(impl(input)).catch(
-        err => `Failed to ${splitWords(name).join(" ")}: ${err}`,
-      );
-      const toToolResult = (res: MaybeArray<LmResponsePart | string>) => {
-        if (!Array.isArray(res)) {
-          res = [res] as Array<LmResponsePart | string>;
-        }
-        res = res.map(res => (typeof res === "string" ? new LanguageModelTextPart(res) : res));
-
-        return new LanguageModelToolResult(res as LmResponsePart[]);
-      };
-      return result.then(toToolResult);
-    },
-  };
-
-  if (params?.invocationMessage || params?.confirmationMessage) {
-    tool.prepareInvocation = options => {
-      const result: PreparedToolInvocation = {};
-      if (params.confirmationMessage) {
-        result["confirmationMessages"] =
-          typeof params.confirmationMessage === "function"
-            ? params.confirmationMessage(options.input)
-            : params.confirmationMessage;
-      }
-      if (params.invocationMessage) {
-        result["invocationMessage"] =
-          typeof params.invocationMessage === "function"
-            ? params.invocationMessage(options.input)
-            : params.invocationMessage;
-      }
-      return result;
-    };
-  }
-
-  return vscode.lm.registerTool(name, tool);
-};
+) =>
+  vscode.lm.registerTool<P>(name, {
+    invoke: ({ input }) =>
+      asPromise(impl(input)).then(toToolResult, err => {
+        throw `Failed to ${splitWords(name)
+          .map(name => name.toLowerCase())
+          .join(" ")}: ${err}`;
+      }),
+    prepareInvocation: ({ input }) => ({
+      confirmationMessages: maybeCall(params?.confirmationMessage, input),
+      invocationMessage: maybeCall(params?.invocationMessage, input),
+    }),
+  });
